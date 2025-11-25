756236514a17974e4809daed0c67aa08da7bf1d4
Roger <wangzhangli@RogerdeMacBook-Pro-2.local>
2025-11-26 02:10:28 +0800

    删除 backend 文件夹和 README.md，只保留 frontend 文件夹

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-import { createServer } from './src/server.js';
-import { config } from './src/config.js';
-
-const app = createServer();
-
-app.listen(config.port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`API listening on http://localhost:${config.port}`);
-});
